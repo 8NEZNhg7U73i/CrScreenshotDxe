eead1115a860e3c36063f74ff1b0faa50a100760
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-04-15 16:05:17 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -104,7 +104,7 @@ ShowStatus (
     )
 {
     // Determines the size of status square
-    #define STATUS_SQUARE_SIDE 100
+    #define STATUS_SQUARE_SIDE 30
 
     UINTN        HandleCount;
     EFI_HANDLE   *HandleBuffer = NULL;
