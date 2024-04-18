abead9e8a3266f8bd8c545c11012564f2606a8f5
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-04-18 16:38:13 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -128,7 +128,7 @@ FindWritableFs (
             }
             
             // Try opening a file for writing
-            if (Fs == Null) {
+            if (Fs == NULL) {
                 Status = Fs->Open(Fs, &File, L"screenshot\\crsdtest.fil", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, 0);
                 if (EFI_ERROR (Status)) {
                     DEBUG((-1, "FindWritableFs: Fs->Open[%d] returned %r\n", i, Status));
