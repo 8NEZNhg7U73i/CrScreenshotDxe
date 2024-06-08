b966e88b3f6acc93eff52476c4fdcb954d53c854
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-08 10:01:31 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -341,7 +341,7 @@ void ReadKeyStroke(IN EFI_EVENT Event, IN VOID *Context)
     EmptyKeyData.Key.UnicodeChar = 0;
     EmptyKeyData.KeyState.KeyShiftState = 0;
     EmptyKeyData.KeyState.KeyToggleState = 0;
-    void Buff = *Context;
+    void Buff = *(VOID*) Context;
     KeyFuncBuff Buff1;
     Buff1 = (KeyFuncBuff) *Buff;
     Status = gST->ConIn->ReadKeyStroke (gST->ConIn, &Key);
