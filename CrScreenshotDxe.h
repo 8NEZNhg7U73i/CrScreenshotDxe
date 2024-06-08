4488e9f65c568275e7cd228bebaf71191baa1250
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-08 05:22:08 +0000

    Update CrScreenshotDxe.h

--- CrScreenshotDxe.h
+++ CrScreenshotDxe.h
@@ -1,9 +1,9 @@
 #define gCrScreenShotGuid { 0x02e4e4f7, 0x38d9, 0x4924, {0xa4, 0xd7, 0xec, 0x6b, 0x69, 0x84, 0x7a, 0xa3 } };
 typedef struct _EFI_CR_SCREEN_SHOT_PROTOCOL  EFI_CR_SCREEN_SHOT_PROTOCOL;
-typedef EFI_STATUS (EFIAPI* CrScreenshotnull)(
+typedef EFI_STATUS (EFIAPI* CR_SCREEN_SHOT_CLASS_NULL)(
   void *null
 );
 
 struct _EFI_CR_SCREEN_SHOT_PROTOCOL {
-  EFI_CR_SCREEN_SHOT_PROTOCOL CrScreenshotnull();
-}
+  CR_SCREEN_SHOT_CLASS_NULL CrScreenshotnull();
+};
