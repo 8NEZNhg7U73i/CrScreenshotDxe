1587f7036e365236e48b784217932d247b7ab644
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-08 09:36:51 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -340,8 +340,8 @@ void ReadKeyStroke(IN EFI_EVENT Event, IN VOID *Context)
     if (!EFI_ERROR (Status)) {
         DEBUG ((-1, "gST->ConIn->ReadKeyStroke Failed: %r\n", Status));
     } else {
-        if ((&Context.KeyInput.UnicodeChar == Key.UnicodeChar) && (&Context.KeyInput.ScanCode == Key.ScanCode)) {
-            (Context->KeyNotificationFunction)(&EmptyKeyData);
+        if (((EFI_KEY_NOTIFY_FUNCTION) &Context.KeyInput.UnicodeChar == Key.UnicodeChar) && (&Context.KeyInput.ScanCode == Key.ScanCode)) {
+            ((Context->KeyNotificationFunction)(&EmptyKeyData);
         }
     }
 }
