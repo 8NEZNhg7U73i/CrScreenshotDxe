635ddeff83e461b9650be0f4d7fcb6ab9fc0b4e7
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-08 15:56:55 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -323,10 +323,10 @@ void ReadKeyStroke (IN EFI_EVENT Event, IN VOID *Context)
     EFI_INPUT_KEY Key;
     KeyFuncBuff *Buff = (VOID *)(Context);
     Print(L"Context: %p\n", Context);
-    Print(L"Context*: %p\n", *(VOID *)Context);
+    Print(L"Context*: %p\n", *(VOID **)Context);
     Print(L"Context&: %p\n", &Context);
     Print(L"Buff: %p\n", Buff);
-    Print(L"Buff*: %p\n", *(VOID *)Buff);
+    Print(L"Buff*: %p\n", *(VOID **)Buff);
     Print(L"Buff&: %p\n", &Buff);
     Status = gST->ConIn->ReadKeyStroke (gST->ConIn, &Key);
     Print(L"ScanCode set: %0X\n", Buff->KeyInput->ScanCode);
