5b424ecd6af45084e8cb2a10325b46eb577b7607
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-09 09:01:40 +0000

    f

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -33,6 +33,7 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <Protocol/GraphicsOutput.h>
 #include <Protocol/SimpleTextInEx.h>
 #include <Protocol/SimpleFileSystem.h>
+#include <Library/MemoryAllocationLib.h>
 
 #include "lodepng.h" //PNG encoding library
 
