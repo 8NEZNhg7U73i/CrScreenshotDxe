4529b29f1e36dcd636b096222f16e5f278c9f5f7
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-09 05:29:08 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -343,6 +343,14 @@ void ReadKeyStroke (IN EFI_EVENT Event, IN VOID *Context)
     Print(L"UnicodeChar set: %c\n", Buff->KeyInput->UnicodeChar);
     Print(L"KeyNotificationFunction set: %s\n", Buff->KeyNotificationFunction);
     Print(L"KeyInput Set: %0X\n", Buff->KeyInput);
+    Print(L"ScanCode set: %0X\n", Buff->KeyInput->ScanCode);
+    Print(L"UnicodeChar set: %c\n", Buff1->KeyInput->UnicodeChar);
+    Print(L"KeyNotificationFunction set: %s\n", Buff1->KeyNotificationFunction);
+    Print(L"KeyInput Set: %0X\n", Buff1->KeyInput);
+    Print(L"ScanCode set: %0X\n", Buff2->KeyInput->ScanCode);
+    Print(L"UnicodeChar set: %c\n", Buff2->KeyInput->UnicodeChar);
+    Print(L"KeyNotificationFunction set: %s\n", Buff2->KeyNotificationFunction);
+    Print(L"KeyInput Set: %0X\n", Buff2->KeyInput);
     /*
     Print(L"ScanCode: %0X\n", Key.ScanCode);
     if (!EFI_ERROR (Status)) {
