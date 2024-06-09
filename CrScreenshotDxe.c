2377c2d4be79787e3ed16de12f06f6ae8f9934e0
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-09 06:19:09 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -576,7 +576,7 @@ CrScreenshotDxeEntry (
             } else {
                 Print (L"CrScreenshotDxeEntry: SimpleTextInWaitForKeyStroke[%d] returned %r\n", Index, Status);
             }
-
+/*
             // Register Right key notification function
             Status = SimpleTextInWaitForKeyStroke (
                     SimpleTextIn,
@@ -616,6 +616,7 @@ CrScreenshotDxeEntry (
                 Print (L"CrScreenshotDxeEntry: SimpleTextInWaitForKeyStroke[%d] returned %r\n", Index, Status);
             }
         }
+*/
 
         // Show success only when we found at least one working implementation
         if (Installed)
