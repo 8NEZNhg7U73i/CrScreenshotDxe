c179408ac6eb48a7e5e1b5a6e76c38fce493ac04
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-09 07:03:39 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -342,7 +342,8 @@ EFI_STATUS EFIAPI SimpleTextInWaitForKeyStroke (
     OUT VOID **NotifyHandle
     )
 {
-    EFI_EVENT TimeEvent;
+    EFI_EVENT TimeEvent[10];
+    static int Eventnum = 0;
     EFI_STATUS Status;
     KeyFuncBuff *Buff = NULL;
     //CHAR16 *Buff1 = L"test\n";
@@ -351,13 +352,13 @@ EFI_STATUS EFIAPI SimpleTextInWaitForKeyStroke (
     Print(L"ScanCode set: %0X\n", Buff->ScanCode);
     Print(L"KeyNotificationFunction set: %s\n", Buff->KeyNotificationFunction);
     //Print(L"KeyInput Set: %0X\n", Buff.KeyInput);
-    Status = gBS->CreateEvent(EVT_TIMER | EVT_NOTIFY_SIGNAL, TPL_NOTIFY, (EFI_EVENT_NOTIFY)ReadKeyStroke, Buff, &TimeEvent);
+    Status = gBS->CreateEvent(EVT_TIMER | EVT_NOTIFY_SIGNAL, TPL_NOTIFY, (EFI_EVENT_NOTIFY)ReadKeyStroke, Buff, &TimeEvent[Eventnum]);
     Print(L"Status: %r\n", Status);
     if (EFI_ERROR (Status)) {
         Print (L"gBS->CreateEvent Failed: %r\n", Status);
         return Status;
     }
-    Status = gBS->SetTimer(TimeEvent, TimerPeriodic, 1 * 1000 * 1000);
+    Status = gBS->SetTimer(TimeEvent[Eventnum], TimerPeriodic, 1 * 1000 * 1000);
     if (EFI_ERROR (Status)) {
         Print (L"gBS->SetTimer Failed: %r\n", Status);
         return Status;
