ba3d2428014614413de29c21ce95845ce6820fdc
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-10 12:39:21 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -257,7 +257,7 @@ TakeScreenshot (
             }
             if (j == ImageSize) {
                 DEBUG((-1, "TakeScreenshot: GraphicsOutput->Blt returned pitch black image, skipped\n"));
-                ShowStatus(0x00, 0x00, 0xFF); //Blue
+                //ShowStatus(0x00, 0x00, 0xFF); //Blue
                 break;
             }
             
