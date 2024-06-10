13e67194459d5b194718d7fd5bed0e2981cb1608
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-10 12:55:11 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -160,8 +160,8 @@ ShowStatus (
         // Draw the status square
         GraphicsOutput->Blt(GraphicsOutput, Square, EfiBltBufferToVideo, 0, 0, 0, 0, STATUS_SQUARE_SIDE, STATUS_SQUARE_SIDE, 0);
         
-        // Wait 250ms
-        gBS->Stall(250*1000);
+        // Wait 100ms
+        gBS->Stall(100*1000);
         
         // Restore the backup
         GraphicsOutput->Blt(GraphicsOutput, Backup, EfiBltBufferToVideo, 0, 0, 0, 0, STATUS_SQUARE_SIDE, STATUS_SQUARE_SIDE, 0);
