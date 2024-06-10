c4ebd976fd7b5fdac360f30887796f4f7988d0ad
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-10 16:35:23 +0000

    Update CrScreenshotDxe.c

--- CrScreenshotDxe.c
+++ CrScreenshotDxe.c
@@ -275,8 +275,8 @@ TakeScreenshot (
                 Image[j].Reserved = 0xFF;
             }
 
-            if (LastImageSize == ImageSize) {
-                Status = CompareMem(LastImage, Image, ImageSize * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
+            if (LastImageSize[i] == ImageSize[i]) {
+                Status = CompareMem(LastImage[i], Image[i], ImageSize * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
                 if (Status == EFI_SUCCESS)
                 {
                     DEBUG((-1, "CompareMem: returned %r\n", Status));
@@ -302,19 +302,19 @@ TakeScreenshot (
             }
 
             gBS->FreePool(LastImage);
-            Status = gBS->AllocatePool(EfiBootServicesData, ImageSize * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL), (VOID **)&LastImage);
+            Status = gBS->AllocatePool(EfiBootServicesData, ImageSize[i] * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL), (VOID **)&LastImage[i]);
             if (EFI_ERROR(Status)) {
                 DEBUG((0, "TakeScreenshot: gBS->AllocatePool returned %r\n", Status));
                 break;
             }
 
-            CopyMem(LastImage, Image, ImageSize * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
+            CopyMem(LastImage[i], Image[i], ImageSize[i] * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
             if (EFI_ERROR(Status)) {
                 DEBUG((0, "TakeScreenshot: CopyMem returned %r\n", Status));
                 break;
             }
 
-            LastImageSize = ImageSize;
+            LastImageSize[i] = ImageSize[i];
 
             // Show success
             ShowStatus(0x00, 0xFF, 0x00); //Green
